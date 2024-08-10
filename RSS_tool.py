7b7221d8a1ff3af440c2a1690f514faf6eb466e1
Lars Dahl <larsosw@gmail.com>
2024-08-10 16:16:43 +0200

    Added functionality to generate html file and to have the tool run every x seconds natively.

--- RSS_tool.py
+++ RSS_tool.py
@@ -2,6 +2,13 @@ import feedparser
 import json
 from functools import reduce
 from operator import getitem
+from json2html import *
+import os as os
+from threading import Event
+from datetime import datetime
+
+exit = Event()
+
 
 realestate_rss_feeds = {
     "E24": "https://e24.no/rss2/?seksjon=eksklusiv24_eiendom",
@@ -62,7 +69,7 @@ RSS_path_config = {
     }
 }
 
-def safe_get(dct, keys):
+def safe_get(entry, keys):
     """
     Safely retrieves the value at nested keys in a dictionary.
     Returns None if any key in keys doesn't exist in dct.
@@ -72,22 +79,86 @@ def safe_get(dct, keys):
     except (KeyError, TypeError):
         return None
     
+
+def get_rss_data(rss = realestate_rss_feeds):
+    parsedict = {}
+
+    # Get the data and put it into json files
+    for feed in rss:
+        # print(feed)
+        d = parsedict[feed] = feedparser.parse(realestate_rss_feeds[feed])
+        all_links = ""
+
+        for i, entry in enumerate(d["entries"]):
+            #print(entry)
+            if entry != {}:
+                json_obj = {
+                    "url": safe_get(entry, RSS_path_config[feed]["url"]),
+                    "title": safe_get(entry, RSS_path_config[feed]["title"]),
+                    "date": safe_get(entry, RSS_path_config[feed]["date"]),
+                    "image": safe_get(entry, RSS_path_config[feed]["image"])
+                }
+                # Save json to storage/
+                output_dir = f"storage/{feed}"
+                os.makedirs(output_dir, exist_ok=True) # First run needs creating the Output folder
+                json_fn = os.path.join(output_dir, f"{i}.json")                
+                with open(json_fn, "w") as file:
+                    json.dump(json_obj, file)
+                all_links += json_obj["url"] + ", "
+        #print(all_links)
+
+
+
+    # getting the timestamp
+    dt = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+
+    # Make html file of feed in Output    
+    output_dir = "Output"
+    os.makedirs(output_dir, exist_ok=True) # First run needs creating the Output folder    
+    html_fn = os.path.join(output_dir, "storage.html")    
+    with open(html_fn, "w") as html_file :
+        html_file.write(f"<title>NewSec media parser!</title>\n")        
+        html_file.write(f"<h1>NewSec media parser, updated {dt}</h1>\n")        
+        for feed in realestate_rss_feeds:
+            d = parsedict[feed]
+            html_file.write(f"<h2>Media: {feed}</h2>\n")        
+            for i, entry in enumerate(d["entries"]):
+                if entry != {}:
+                    for f in ["date"]:
+                        data = str(safe_get(entry, RSS_path_config[feed][f]))
+                        html_file.write(f'<p>{f}: {data}, ')
+                    for f in ["title"]:
+                        data = str(safe_get(entry, RSS_path_config[feed][f]))
+                        url  = str(safe_get(entry, RSS_path_config[feed]["url"]))
+                        html_file.write(f'<a href="{url}">{data}</a>, ')
+                    for f in ["image"]:
+                        data = "Illustration, image"
+                        url  = str(safe_get(entry, RSS_path_config[feed]["image"]))
+                        html_file.write(f'{f}: <a href="{url}">{data}</a></p>\n')
+
+
+
+    print(f"NewSec media parser Updated rss data to {html_fn} at {dt}")
+
+
+def main():
+    while not exit.is_set():
+      get_rss_data()
+      exit.wait(20)
+
+    print("All done!")
+    # perform any cleanup here
+
+def quit(signo, _frame):
+    print("Interrupted by %d, shutting down" % signo)
+    exit.set()
+
+if __name__ == '__main__':
+    import signal
+    for sig in ('TERM', 'HUP', 'INT'):
+        signal.signal(getattr(signal, 'SIG'+sig), quit);
+
+    main()
+
+
     
-for feed in realestate_rss_feeds:
-    print(feed)
-    d = feedparser.parse(realestate_rss_feeds[feed])
-    all_links = ""
-    for i, entry in enumerate(d["entries"]):
-        #print(entry)
-        if entry != {}:
-            json_obj = {
-                "url": safe_get(entry, RSS_path_config[feed]["url"]),
-                "title": safe_get(entry, RSS_path_config[feed]["title"]),
-                "date": safe_get(entry, RSS_path_config[feed]["date"]),
-                "image": safe_get(entry, RSS_path_config[feed]["image"])
-            }
-            # Save json to storage/
-            with open(f"storage/{feed}/{i}.json", "w") as file:
-                json.dump(json_obj, file)
-            all_links += json_obj["url"] + ", "
-    print(all_links)
